5c1d4aa3f28be90613ca96d6efd368611484d780
Ed Kubiak <ek33450505@gmail.com>
2022-05-09 15:27:27 -0400

    added hooks for password hashing

--- models/User.js
+++ models/User.js
@@ -1,24 +1,22 @@
 const { Model, DataTypes } = require('sequelize');
 const sequelize = require('../config/connection');
+const bcrypt = require('bcrypt');
 
 // create our User model
 class User extends Model {}
 
 User.init(
     {
-      // define an id column
       id: {
         type: DataTypes.INTEGER,
         allowNull: false,
         primaryKey: true,
         autoIncrement: true
       },
-      // define a username column
       username: {
         type: DataTypes.STRING,
         allowNull: false
       },
-      // define an email column
       email: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -27,7 +25,6 @@ User.init(
           isEmail: true
         }
       },
-      // define a password column
       password: {
         type: DataTypes.STRING,
         allowNull: false,
@@ -37,6 +34,18 @@ User.init(
       }
     },
     {
+        hooks: {
+            // set up beforeCreate lifecycle "hook" functionality
+            async beforeCreate(newUserData) {
+              newUserData.password = await bcrypt.hash(newUserData.password, 10);
+              return newUserData;
+            },
+            // set up beforeUpdate lifecycle "hook" functionality
+            async beforeUpdate(updatedUserData) {
+                updatedUserData.password = await bcrypt.hash(updatedUserData.password, 10);
+                return updatedUserData
+            }
+      },
       sequelize,
       timestamps: false,
       freezeTableName: true,
