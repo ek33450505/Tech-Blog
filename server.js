f2ee2b5cca2a1533374a6f17957b4566ed4ba303
Ed Kubiak <ek33450505@gmail.com>
2022-05-12 16:11:52 -0400

    added homepage functionality - need to work on css and html elements

--- server.js
+++ server.js
@@ -1,17 +1,25 @@
 const express = require('express');
-const routes = require('./routes');
+const routes = require('./controllers/');
 const sequelize = require('./config/connection');
+const path = require('path');
+const exphbs = require('express-handlebars');
 
 const app = express();
 const PORT = process.env.PORT || 3001;
 
+const hbs = exphbs.create({});
+
+app.engine('handlebars', hbs.engine);
+app.set('view engine', 'handlebars');
+
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
-
+// The express.static() method is a built-in Express.js middleware function that can take all of the contents of a folder and serve them as static assets
+app.use(express.static(path.join(__dirname, 'public')));
 // turn on routes
 app.use(routes);
 
-// turn on connection to db and server
+// turn on connection to database and server
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening'));
 });
