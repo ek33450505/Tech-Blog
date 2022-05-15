d0dec5235d2327a0da170995abc0c0f33b4d0cbe
Ed Kubiak <ek33450505@gmail.com>
2022-05-15 17:30:48 -0400

    corrected user model

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -1,10 +1,10 @@
 const router = require('express').Router();
 const sequelize = require('../config/connection');
-const { Post, User, Comment, Vote } = require('../models');
+const { Post, User, Comment } = require('../models');
 
 // get all posts for homepage
 router.get('/', (req, res) => {
-  console.log('======================');
+  console.log('req.session');
   Post.findAll({
     attributes: [
       'id',
